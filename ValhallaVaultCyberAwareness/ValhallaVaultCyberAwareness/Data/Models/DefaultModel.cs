f961866c7ec03191c76ecb691b7ef2363bb2a4df
Emil Åberg <61052905+Emilaberg@users.noreply.github.com>
2024-03-04 10:42:30 +0100

    Add project files.

---
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Models/DefaultModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValhallaVaultCyberAwareness.Data.Models
+{
+    //En vanlig modell som gör absolut ingenting.
+    public class DefaultModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
