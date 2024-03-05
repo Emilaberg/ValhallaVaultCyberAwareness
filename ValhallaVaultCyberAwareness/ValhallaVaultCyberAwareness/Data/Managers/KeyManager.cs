b2dcb4f545d5d6c07014ab55f0d4cdcdff0d7827
Emil Åberg <61052905+Emilaberg@users.noreply.github.com>
2024-03-05 10:05:28 +0100

    Update KeyManager.cs

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Managers/KeyManager.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Managers/KeyManager.cs
@@ -2,7 +2,8 @@
 {
     public class KeyManager
     {
-        private readonly string filepath = "C:\\Users\\Skola\\Desktop\\safe.txt";
+        //vägen till filen där ni har connectionstring
+        private readonly string filepath = "C:\\väg\\till\\er\\fil.txt";
         public string? GetKey()
         {
             return File.Exists(filepath) ? File.ReadAllText(filepath) : null;
