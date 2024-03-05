ac767a3e39018fcaff3ab8f1e52d1bf7ee065efc
Gustav-Frising <143353638+Gustav-Frising@users.noreply.github.com>
2024-03-05 15:23:35 +0100

    Update KeyManager.cs
    problem med git.ignore

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Managers/KeyManager.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Managers/KeyManager.cs
@@ -3,7 +3,7 @@
     public class KeyManager
     {
         //vägen till filen där ni har connectionstring
-        private readonly string filepath = "C:\\Users\\Skola\\Desktop\\safe.txt";
+        private readonly string filepath = "C:\\väg\\till\\er\\fil.txt";
         public string? GetKey()
         {
             return File.Exists(filepath) ? File.ReadAllText(filepath) : null;
