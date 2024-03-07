29310f76ef46c2b242b675b9ebe2618aa48a4900
Gustav-Frising <frising96@gmail.com>
2024-03-07 14:48:21 +0100

    Code-Cleanup

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Repositories/ValhallaUow.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Repositories/ValhallaUow.cs
@@ -4,31 +4,6 @@ namespace ValhallaVaultCyberAwareness.Repositories
 {
     public class ValhallaUow : IValhallaUow
     {
-        //private readonly ApplicationDbContext _context;
-
-        //public CategoryRepository CategoryRepo { get; }
-        //public SegmentRepository SegmentRepo { get; }
-        //public SubCategoryRepository SubCategoryRepo { get; }
-        //public QuestionRepository QuestionRepo { get; }
-        //public PromptRepository PromptRepo { get; }
-
-        //public ValhallaUow(ApplicationDbContext context)
-        //{
-
-        //    _context = context;
-
-        //    CategoryRepo = new(context);
-        //    SegmentRepo = new(context);
-        //    SubCategoryRepo = new(context);
-        //    QuestionRepo = new(context);
-        //    PromptRepo = new(context);
-
-        //}
-        //public async Task CompleteAsync()
-        //{
-        //    await _context.SaveChangesAsync();
-
-        //}
         public CategoryRepository CategoryRepo { get; }
 
         public SegmentRepository SegmentRepo { get; }
