a45549b818ae1ac9cdeffb96426b3c5bc33476c3
Gustav-Frising <frising96@gmail.com>
2024-03-14 22:18:05 +0100

    admin-page

---
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ValhallaVaultCyberAwareness.Data.Models;
+
+namespace ValhallaVaultCyberAwareness.Data.Repositories
+{
+    public class CategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<CategoryModel>> GetAllCategories()
+        {
+            var categories = await _context.Categories.ToListAsync();
+
+            return categories;
+
+        }
+        public async Task<CategoryModel> GetCategoryById(int id)
+        {
+            var singleCategory = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (singleCategory != null)
+            {
+                return singleCategory;
+            }
+            throw new NullReferenceException();
+        }
+        public async Task<CategoryModel> AddCategory(CategoryModel newCategory)
+        {
+            if (newCategory != null)
+            {
+                _context.Add(newCategory);
+                await _context.SaveChangesAsync();
+
+                return newCategory;
+            }
+            throw new NullReferenceException();
+        }
+
+        public async Task<CategoryModel> DeleteCategory(int id)
+        {
+            var categoryToDelete = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (categoryToDelete != null)
+            {
+                _context.Categories.Remove(categoryToDelete);
+
+                await _context.SaveChangesAsync();
+                return categoryToDelete;
+            }
+            throw new NullReferenceException();
+        }
+        public async Task<CategoryModel> UpdateCategory(CategoryModel updatedCategory)
+        {
+            var categoryToUpdate = await _context.Categories.FirstOrDefaultAsync(c => c.Id == updatedCategory.Id);
+
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.CategoryName = updatedCategory.CategoryName;
+
+                await _context.SaveChangesAsync();
+                return updatedCategory;
+            }
+            throw new NullReferenceException();
+        }
+
+
+    }
+
+
+
+}
+
---
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Repositories/QuestionRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ValhallaVaultCyberAwareness.Data;
+using ValhallaVaultCyberAwareness.Data.Models;
+
+namespace ValhallaVaultCyberAwareness.Repositories
+{
+    public class QuestionRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public QuestionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<QuestionModel>> GetAllQuestions()
+        {
+            var Questions = await _context.Questions.ToListAsync();
+
+            return Questions;
+
+        }
+        public async Task<QuestionModel> GetQuestionById(int id)
+        {
+            var singleQuestion = await _context.Questions.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (singleQuestion != null)
+            {
+                return singleQuestion;
+            }
+            throw new NullReferenceException();
+        }
+        public async Task<QuestionModel> AddQuestion(QuestionModel newQuestion)
+        {
+            if (newQuestion != null)
+            {
+                _context.Add(newQuestion);
+                await _context.SaveChangesAsync();
+
+                return newQuestion;
+            }
+            throw new NullReferenceException();
+        }
+
+        public async Task<QuestionModel> DeleteQuestion(int id)
+        {
+            var QuestionToDelete = await _context.Questions.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (QuestionToDelete != null)
+            {
+                _context.Questions.Remove(QuestionToDelete);
+
+                await _context.SaveChangesAsync();
+                return QuestionToDelete;
+            }
+            throw new NullReferenceException();
+        }
+        public async Task<QuestionModel> UpdateQuestion(QuestionModel updatedQuestion)
+        {
+            var QuestionToUpdate = await _context.Questions.FirstOrDefaultAsync(c => c.Id == updatedQuestion.Id);
+
+            if (QuestionToUpdate != null)
+            {
+                QuestionToUpdate.Question = updatedQuestion.Question;
+
+                await _context.SaveChangesAsync();
+                return updatedQuestion;
+            }
+            throw new NullReferenceException();
+        }
+
+
+    }
+}
