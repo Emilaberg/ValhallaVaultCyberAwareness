3763c2a64164980d4174d6c77c96122e625a8886
Gustav-Frising <frising96@gmail.com>
2024-03-17 22:16:43 +0100

    Middleware comments

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Midleware/TimeMiddleware.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Midleware/TimeMiddleware.cs
@@ -11,10 +11,11 @@
             _logger = logger;
             _next = next;
         }
+        //Loggs the request path and time in milliseconds
         public async Task Invoke(HttpContext ctx)
         {
             var start = DateTime.Now;
-            await _next.Invoke(ctx);
+            await _next.Invoke(ctx);//pass the context in the pipeline
             _logger.LogInformation($"time : {ctx.Request.Path} :{(DateTime.Now - start).TotalMilliseconds}ms");
         }
 
