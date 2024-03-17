51b57cc12a56466a80de108126f2196c95a0eb37
Gustav-Frising <frising96@gmail.com>
2024-03-17 19:12:48 +0100

    Added-Reqired-on-Models

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Models/SegmentModel.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Data/Models/SegmentModel.cs
@@ -7,6 +7,8 @@ namespace ValhallaVaultCyberAwareness.Data.Models
         [Key]
         public int Id { get; set; }
         // Namnet för själva segmentet, som "Att skydda sig mot bedrägerier del 1 "
+        [Required(ErrorMessage = "Segment name is required")]
+        [MinLength(4, ErrorMessage = "Segment name must be at least 4 characters long")]
         public string? SegmentName { get; set; }
         public int CategoryId { get; set; }
         public CategoryModel Category { get; set; } = null!;
