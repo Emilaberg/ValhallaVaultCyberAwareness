e80b8b1e520d29270c2481d550cb51eb59410094
Emil Åberg <61052905+Emilaberg@users.noreply.github.com>
2024-04-11 22:15:04 +0200

    unit test tillagt och questionpage

---
+++ ValhallaVault_Testing/Emil-testklasser/TestQuestionLogic.cs
@@ -0,0 +1,43 @@
+﻿using ValhallaVaultCyberAwareness.Components.Pages;
+namespace ValhallaVault_Testing.Emil_testklasser
+{
+    public class TestQuestionLogic
+    {
+
+
+        [Fact]
+        public void TestLoadNextQuestion()
+        {
+            Questions questionpage = new Questions();
+            int beforeNextQuestionInt = questionpage.currentQuestion;
+
+            questionpage.NextQuestion();
+
+            Assert.Equal(beforeNextQuestionInt + 1, questionpage.currentQuestion);
+        }
+
+        [Fact]
+        public void TestCurrentQuestionShouldBeTheSameWhenPressingPrevQuestion()
+        {
+            Questions questionpage = new Questions();
+            int beforeNextQuestionInt = questionpage.currentQuestion;
+
+            questionpage.PrevQuestion();
+
+            Assert.Equal(beforeNextQuestionInt, questionpage.currentQuestion);
+        }
+
+        [Fact]
+        public void TestCantCheckAnswerWhenNotSelectedAnAnswer()
+        {
+            Questions questionpage = new Questions();
+
+            Assert.Null(questionpage.answeredPrompt);
+
+            questionpage.CheckAnswer();
+
+            Assert.Null(questionpage.answeredPrompt);
+            Assert.False(questionpage.answeredCorrect);
+        }
+    }
+}
